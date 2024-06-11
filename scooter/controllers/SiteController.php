214abcf9738f4929d113ccd2a205a3b06c951ab6
arosl <Yaracc1@mail.ru>
2024-06-11 18:28:01 +0400

    scooter1

---
+++ scooter/controllers/SiteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+}
+
---
+++ scooter/views/site/about.php
@@ -0,0 +1,40 @@
+<?php
+/** @var yii\web\View $this */
+
+$this->title = 'Магазин экстримального вида спорта';
+?>
+
+
+<!-- breadcrumb part start-->
+<section class="breadcrumb_part">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb_iner">
+                    <h2>About</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- breadcrumb part end-->
+
+<!-- product list part start-->
+<section class="about_us padding_top">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="about_us_content">
+                    <h5>Our Mission</h5>
+                    <h3>Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed neque.</h3>
+                    <div class="about_us_video">
+                        <img src="img/about_us_video.png" alt="#" class="img-fluid">
+                        <a class="about_video_icon popup-youtube" href="https://www.youtube.com/watch?v=DWHB6nTyKDI"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- product list part end-->
+
